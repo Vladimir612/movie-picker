fe1c3b094522213996d1a1706fa567a23093664a
unknown <vladimir.12.lazarevic@gmail.com>
2021-03-30 15:45:03 +0200

    Fixed problem that I cause with last commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from './pages/Home'
 import Movie from './pages/Movie'
 import Actor from './pages/Actor'
 import Error from './pages/Error'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import { SingleMovieProvider } from './data/SingleMovieContext'
 
 const App = () => {
@@ -12,8 +12,7 @@ const App = () => {
         <Route exact path='/' component={Home} />
         <Route exact path='/movies/:movie' component={Movie} />
         <Route exact path='/actors/:actor' component={Actor} />
-        <Route exact path='/404' component={Error} />
-        <Redirect to='/404' />
+        <Route path='*' component={Error} />
       </SingleMovieProvider>
     </Switch>
   )
