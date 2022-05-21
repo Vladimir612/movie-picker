b184011658f4143a8f327bdc7c3f2962478da14b
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2022-05-22 01:14:29 +0200

    Added some things so it can work on new version of libraries

--- src/data/MoviesContext.js
+++ src/data/MoviesContext.js
@@ -84,11 +84,9 @@ export const MoviesProvider = ({ children }) => {
     setLoading(true)
 
     let finalUrl = movieOnKeywordUrl + keyword
-    console.log(finalUrl)
     fetch(finalUrl)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
         setMovies(formatMovies(data.results))
       })
       .then(
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,46 +1,47 @@
-import React, { useEffect, useRef } from 'react'
-import TopHeader from './../../Components/TopHeader'
-import './home.sass'
-import MoviesFilter from '../../Components/MoviesFilter'
-import Movies from '../../Components/Movies'
-import Footer from '../../Components/Footer'
-import { GenresProvider } from '../../data/GenresContext'
-import { MoviesProvider } from '../../data/MoviesContext'
-import { useScroll } from '../../data/ScrollContext'
-import { ActorsProvider } from '../../data/ActorsContext'
-import Actors from '../../Components/Actors'
+import React, { useEffect, useRef } from "react";
+import TopHeader from "./../../Components/TopHeader";
+import "./home.sass";
+import MoviesFilter from "../../Components/MoviesFilter";
+import Movies from "../../Components/Movies";
+import Footer from "../../Components/Footer";
+import { GenresProvider } from "../../data/GenresContext";
+import { MoviesProvider } from "../../data/MoviesContext";
+import { useScroll } from "../../data/ScrollContext";
+import { ActorsProvider } from "../../data/ActorsContext";
+import Actors from "../../Components/Actors";
 
 const Home = () => {
-  let { shouldScroll, positionTo } = useScroll()
-  const firstSection = useRef()
-  const secondSection = useRef()
+  let { shouldScroll, positionTo } = useScroll();
+  const firstSection = useRef();
+  const secondSection = useRef();
+
   const scrollFunc = () => {
     if (shouldScroll && positionTo === 1) {
       firstSection.current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-        inline: 'end',
-      })
+        behavior: "smooth",
+        block: "start",
+        inline: "end",
+      });
     }
     if (shouldScroll && positionTo === 2) {
       secondSection.current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-        inline: 'end',
-      })
+        behavior: "smooth",
+        block: "start",
+        inline: "end",
+      });
     }
-  }
+  };
 
   useEffect(() => {
     setTimeout(() => {
-      scrollFunc()
-    }, 1000)
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+      scrollFunc();
+    }, 1000);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className='home-page'>
-      <TopHeader firstText='Movie' secondText='Picker' left='60%' />
-      <section className='movies-section' ref={firstSection}>
+    <div className="home-page">
+      <TopHeader firstText="Movie" secondText="Picker" left="60%" />
+      <section className="movies-section" ref={firstSection}>
         <MoviesProvider>
           <GenresProvider>
             <MoviesFilter />
@@ -48,16 +49,16 @@ const Home = () => {
           <Movies />
         </MoviesProvider>
       </section>
-      <section className='actors-section' ref={secondSection}>
+      <section className="actors-section" ref={secondSection}>
         <ActorsProvider>
           <Actors />
         </ActorsProvider>
       </section>
-      <section className='footer-section'>
+      <section className="footer-section">
         <Footer />
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
